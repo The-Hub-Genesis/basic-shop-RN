8574533be653770ac8d788f034c52876bb8d9209
knee-rel <nirelmarieibarra@gmail.com>
2021-05-22 09:06:34 +0800

    product items

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import ShopNavigator from "./navigation/ShopNavigator";
+
+import productsReducer from "./store/reducers/products";
+
+const rootReducer = combineReducers({
+  products: productsReducer,
+});
+
+const store = createStore(rootReducer);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Basic Shop App</Text>
-    </View>
+    <Provider store={store}>
+      <ShopNavigator />
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ constants/Colors.js
@@ -0,0 +1,5 @@
+export default {
+  primary: "#181725",
+  secondary: "#53B175",
+  inactive: "#7C7C7C",
+};
